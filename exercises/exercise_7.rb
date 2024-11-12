190d723f668859e4385dba3b832dde83629c5ec1
amusictheorist <a_millaresthomson@outlook.com>
2024-11-12 09:56:20 -0500

    Solve exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,16 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Error creating store:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
