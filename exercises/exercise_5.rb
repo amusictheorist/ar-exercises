4545fb18a0f73edb92e86389d829dfefc8db1825
amusictheorist <a_millaresthomson@outlook.com>
2024-11-12 09:53:48 -0500

    Solve exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,7 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = Store.average(:annual_revenue)
+@large_stores = Store.where("annual_revenue >= ?", 1000000)
+puts @large_stores.count
